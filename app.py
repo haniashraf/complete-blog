f6af64ab915c0d3f653efbfb5ee869f5a0c22156
haniashraf <ashrafshahreyar@icloud.com>
2019-05-29 18:27:40 +0300

    done

---
+++ app.py
@@ -0,0 +1,196 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+# from data import Articles
+from  flask_mysqldb import MySQL
+from wtforms import Form, StringField, PasswordField, TextAreaField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = "hani1234"
+
+#config MYSQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Guddu911'
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init mysql
+mysql = MySQL(app)
+
+# Articles = Articles()
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+	# create cursor
+	cur = mysql.connection.cursor()
+
+	# execute 
+	result = cur.execute('SELECT * FROM articles')
+	articles = cur.fetchall()
+
+	if result > 0:
+		return render_template('articles.html', articles=articles)
+	else:
+		msg = "No Articles Found"
+		return render_template('articles.html', msg=msg)
+
+@app.route('/article/<string:id>/')
+def article(id):
+	# create cursor
+	cur = mysql.connection.cursor()
+
+	# execute 
+	result = cur.execute('SELECT * FROM articles WHERE id = %s', [id])
+	article = cur.fetchone()
+	return render_template('article.html', article=article)
+
+#WTF registration form
+class RegistrationForm(Form):
+	"""docstring for RegistrationForm"""
+	name = StringField('Name', [validators.Length(min=1, max=50)])
+	username = StringField('Username', [validators.Length(min=4, max=25)])
+	email = StringField('Email', [validators.Length(min=6, max=50)])
+	password = PasswordField('Password', [
+		validators.DataRequired(),
+		validators.EqualTo('confirm', message="Passwords Dont match")])
+	confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm(request.form)
+	if request.method == 'POST' and form.validate():
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		#create cursor
+		cur = mysql.connection.cursor()
+
+		cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s,%s,%s,%s)", (name,email,username,password))
+
+		#commit to db
+		mysql.connection.commit()
+
+		#closing cur
+		cur.close()
+
+		flash('You are now register and login', 'success')
+		return redirect(url_for('home'))
+
+	return render_template('register.html', form=form)
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+	    # Get Form Fields
+	    username = request.form['username']
+	    password_candidate = request.form['password']
+
+	    # Create cursor
+	    cur = mysql.connection.cursor()
+
+	    # Get user by username
+	    result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+	    if result > 0:
+	        # Get stored hash
+	        data = cur.fetchone()
+	        password = data['password']
+
+	        # Compare Passwords
+	        if sha256_crypt.verify(password_candidate, password):
+	            # Passed
+	            session['logged_in'] = True
+	            session['username']  = username
+
+	            flash('Logged In', 'success')
+	            return redirect(url_for('dashboard'))
+	        else:
+	        	error = "Invalid Password"
+	        	return render_template('login.html',error=error)
+	        # Close connection
+	        cur.close()
+	    else:
+	    	error = 'Username Not found'
+	    	return render_template('login.html', error=error)
+
+	return render_template('login.html')
+
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args,**kwargs)
+		else:
+			flash('Unauthorized Please Sign In', 'danger')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/logout') 
+@is_logged_in
+def logout():
+	session.clear()
+	flash('You Have Been Logged Out', 'success')
+	return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+	# create cursor
+	cur = mysql.connection.cursor()
+
+	# execute 
+	result = cur.execute('SELECT * FROM articles')
+	articles = cur.fetchall()
+
+	if result > 0:
+		return render_template('dashboard.html', articles=articles)
+	else:
+		msg = "No Articles Found"
+		return render_template('dashboard.html', msg=msg)
+
+#Article Registration 
+class ArticleForm(Form):
+	"""docstring for ArticleForm"""
+	title = StringField("Title", [validators.Length(min=5, max=200)])
+	body = TextAreaField("Body", [validators.Length(min=20)])
+
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+	form = ArticleForm(request.form)
+	if request.method == 'POST' and form.validate():
+		title = form.title.data
+		body = form.body.data
+
+		# Create Cursor
+		cur = mysql.connection.cursor()
+
+		# executing Cursor
+
+		cur.execute("INSERT INTO articles(title, body, author) VALUES(%s,%s,%s)" , (title, body, session['username']))
+
+		# commit connection to db
+		mysql.connection.commit()
+
+		# closing connection
+		cur.close()
+
+		flash('Article Created', 'success')
+
+		return redirect(url_for('dashboard'))
+	return render_template('add_article.html', form=form)
+	
+if __name__ == '__main__': 
+	app.run()
---
+++ data.py
@@ -0,0 +1,23 @@
+def Articles():
+	articles = [
+		{
+		'id':1,
+		'title':'Article 1',
+		'body':'Ea consequat est voluptate reprehenderit ut enim labore dolore in fugiat commodo tempor laborum ullamco commodo reprehenderit excepteur cupidatat excepteur deserunt mollit nulla mollit sed nostrud nisi deserunt officia ex commodo duis fugiat veniam commodo deserunt dolore labore enim ex reprehenderit ut in laboris nostrud sed consectetur mollit voluptate ut anim anim enim culpa duis laborum proident magna occaecat culpa fugiat id eiusmod minim fugiat nisi ut tempor anim anim amet cupidatat culpa est sint in officia est irure dolore dolore in consectetur minim eu incididunt adipisicing aliqua non ullamco sunt in in minim pariatur qui esse proident fugiat proident dolor ullamco magna incididunt qui officia nisi ut eiusmod amet velit velit anim officia veniam ad dolor incididunt quis in ex et sit minim cupidatat ea culpa proident in do exercitation aute nisi non fugiat non irure culpa eu exercitation dolor nisi in ut labore in commodo do proident ullamco dolor enim et duis dolor velit est deserunt irure mollit nisi adipisicing ea culpa reprehenderit excepteur proident ullamco aliquip nulla deserunt cupidatat officia nulla consectetur irure elit ut est incididunt eu duis dolore enim in eu qui nisi sunt culpa eiusmod occaecat ut excepteur in ut commodo in ex cupidatat ullamco in mollit ut mollit minim magna in laboris aliquip in id deserunt sed dolore dolore dolore aliqua adipisicing nisi velit tempor incididunt commodo ut minim eiusmod esse pariatur dolore reprehenderit culpa incididunt amet ea reprehenderit qui aliqua pariatur et aliqua aute et officia velit ullamco sint sunt pariatur excepteur tempor anim ullamco occaecat dolore ad consequat.',
+		'author':'Sheri',
+		'create_date':'04/12/2003'
+		},
+		{
+
+		'id': 2,
+		'title':'Article 2',
+		'body':'Ea consequat est voluptate reprehenderit ut enim labore dolore in fugiat commodo tempor laborum ullamco commodo reprehenderit excepteur cupidatat excepteur deserunt mollit nulla mollit sed nostrud nisi deserunt officia ex commodo duis fugiat veniam commodo deserunt dolore labore enim ex reprehenderit ut in laboris nostrud sed consectetur mollit voluptate ut anim anim enim culpa duis laborum proident magna occaecat culpa fugiat id eiusmod minim fugiat nisi ut tempor anim anim amet cupidatat culpa est sint in officia est irure dolore dolore in consectetur minim eu incididunt adipisicing aliqua non ullamco sunt in in minim pariatur qui esse proident fugiat proident dolor ullamco magna incididunt qui officia nisi ut eiusmod amet velit velit anim officia veniam ad dolor incididunt quis in ex et sit minim cupidatat ea culpa proident in do exercitation aute nisi non fugiat non irure culpa eu exercitation dolor nisi in ut labore in commodo do proident ullamco dolor enim et duis dolor velit est deserunt irure mollit nisi adipisicing ea culpa reprehenderit excepteur proident ullamco aliquip nulla deserunt cupidatat officia nulla consectetur irure elit ut est incididunt eu duis dolore enim in eu qui nisi sunt culpa eiusmod occaecat ut excepteur in ut commodo in ex cupidatat ullamco in mollit ut mollit minim magna in laboris aliquip in id deserunt sed dolore dolore dolore aliqua adipisicing nisi velit tempor incididunt commodo ut minim eiusmod esse pariatur dolore reprehenderit culpa incididunt amet ea reprehenderit qui aliqua pariatur et aliqua aute et officia velit ullamco sint sunt pariatur excepteur tempor anim ullamco occaecat dolore ad consequat.',
+		'author':'Hani',
+		'create_date':'03/1/2013'
+
+
+		}
+	]
+
+
+	return articles
